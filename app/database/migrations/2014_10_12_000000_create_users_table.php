95c626984bb4483a631bb0fb63b19e4b602e09f8
shimura0227 <kikyou765@gmail.com>
2025-01-27 00:15:43 +0900

    20250127 - ヘッダー - 実装中

---
+++ app/database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name',255);
+            $table->string('email',255)->unique();
+            $table->string('password',255);
+            $table->string('image')->nullable();
+            $table->text('profile')->nullable();
+            $table->timestamps();
+            $table->softDeletes()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+        });
+    }
+}
